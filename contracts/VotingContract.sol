6dbdcea491261b614b252bf3263a7d9b91162de3
Vrush2004 <vrushalichaube@gmail.com>
2024-10-06 11:42:12 +0530

    voting contract

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -31,5 +31,62 @@ contract Create{
         uint256 voteCount,
         address _address,
         string ipfs
-    )
+    );
+
+    address[] public candidateAddress;
+
+    mapping(address => Candidate) public candidates;
+
+    // END OF CANDIDATE DATA
+
+    address[] public votedVoters;
+    address[] public votersAddress;
+    mapping(address => Voter) public voters;
+
+    struct Voter{
+        uint256 voter_voterId;
+        string voter_name;
+        string voter_image;
+        address voter_address;
+        uint256 voter_allowed;
+        bool voter_voted;
+        uint256 voter_vote;
+        string voter_ipfs;
+    }
+
+    event VoterCreated(
+        uint256 indexed voter_voterId,
+        string voter_name,
+        string voter_image,
+        address voter_address,
+        uint256 voter_allowed,
+        bool voter_voted,
+        uint256 voter_vote,
+        string voter_ipfs
+    );
+
+    // END OF VOTER DATA
+
+    constructor (){
+        votingOrganizer = msg.sender;
+    }
+
+    function setCandidate(address _address, string memory _age, string memory _name, string memory _image, string memory _ipfs) public{
+        require(
+            votingOrganizer == msg.sender, 
+            "Only organizer can authorized candidate"
+        );
+        _candidateId.increment();
+        uint256 idNumber = _candidateId.current();
+
+        Candidate storage candidate = candidates[_address];
+
+        candidate.age = _age;
+        candidate.name = _name;
+        candidate.candidateId = idNumber;
+        candidate.image = _image;
+        candidate.voteCount = 0;
+        candidate.address = _address;
+    }
+    
 }
