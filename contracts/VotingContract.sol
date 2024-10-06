59ea459b451988f2673d3a9acc4a3c095f93aafc
Vrush2004 <vrushalichaube@gmail.com>
2024-10-06 11:24:54 +0530

    voting contract

---
+++ contracts/VotingContract.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzepplelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+contract Create{
+    using Counters for Counters.Counter
+
+    Counters.Counter public _voterId;
+    Counters.Counter public _candidateId;
+
+    address public votingOrganizer;
+
+    //CANDIDATE FOR VOTING 
+    struct Candidate{
+        uint256 candidateId;
+        string age;
+        syring name;
+        string image;
+        uint256 voteCount;
+        address _address;
+        string ipfs;
+    }
+
+    event CandidateCreate(
+        uint256 indexed candidateId,
+        string age,
+        syring name,
+        string image,
+        uint256 voteCount,
+        address _address,
+        string ipfs
+    )
+}
