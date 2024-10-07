ecaf1e7abaedb4fc0f6dc0272a7c3c82a64638c9
Vrush2004 <vrushalichaube@gmail.com>
2024-10-07 17:30:50 +0530

    allowed voters

--- pages/allowed-voters.js
+++ pages/allowed-voters.js
@@ -11,7 +11,50 @@ import Button from '../components/Button/Button';
 import Input from '../components/Input/Input';
 
 const allowedVoters = () => {
-  
+  const [fileUrl, setFileUrl] = useState(null);
+  const [formInput, setFormInput] = useState({
+    name : "",
+    address: "",
+    position: "",
+  });
+
+  const router = useRouter();
+  const {uploadToIPFS} = useContext(VotingContext)
+
+  // VOTERS IMAGE DROP
+  const onDrop = useCallback(async(acceptedFill) => {
+    const url = await uploadToIPFS(acceptedFill[0]);
+    setFileUrl(url);
+  });
+
+  const {getRootProps, getInputProps} = useDropzone({
+    onDrop,
+    accept: "image/*",
+    maxSize: 5000000,
+  })
+
+  // JSX PART
+  return (
+    <div className={Style.createVoter}>
+        <div>
+            {fileUrl && (
+              <div className={Style.voterInfo}>
+                  <img src={ fileUrl } alt='Voter Image'/>
+                  <div className={Style.voterInfo_paragraph}>
+                      <p>
+                        Name: <span>&nbps; {formInput.name}</span>
+                      </p>
+                      <p>
+                        Address: &nbps;<span> {formInput.address.slice(0,20)}</span>
+                      </p>
+                      <p>
+                        Pos: &nbps;<span> {formInput.address.position}</span>
+                      </p>
+                  </div>
+              </div>
+            )}
+
+
 };
 
 export default allowedVoters;
