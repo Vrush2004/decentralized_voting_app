b7185961081a1b8f5642cf88c6f05ba799f6b0fb
Vrush2004 <vrushalichaube@gmail.com>
2024-10-07 17:31:22 +0530

    allowed voters

--- pages/allowed-voters.js
+++ pages/allowed-voters.js
@@ -53,7 +53,67 @@ const allowedVoters = () => {
                   </div>
               </div>
             )}
+            {
+              !fileUrl && (
+                <div className='Style.sideInfo'>
+                  <div className='Style.sideInfo_box'>
+                      <h4>Create Candidate For Voting</h4>
+                      <p>
+                        Blockchain voting organition, provide ethereum Ecosystem !!!
+                      </p>
+                      <p className={Style.sideInfo_para}>Contract Candidate</p>
+                  </div>
+                  <div className={Style.car}>
+                      {/* {voterArray.map((el,i) => (
+                          <div key={i+1} className={Style.card_box}>
+                              <div className={Style.image}>
+                                <img src="" alt="Profile photo" />
+                              </div>
 
+                              <div className={Style.card_info}>
+                                <p>Name</p>
+                                <p>Address</p>
+                                <p>Details</p>
+                              </div>
+                          </div>
+                      ))} */}
+                  </div>
+                </div>
+              )
+            }
+        </div>
+
+        <div className={Style.voter}>
+            <div className={Style.voter__container}>
+                <h1>Create New Voter</h1>
+                <div className={Style.voter__container__box}>
+                    <div className={Style.voter__container__box__div}>
+                        <div {...getRootProps()}>
+                            <input {...getInputProps()}/>
+
+                            <div className={Style.voter__container__box__div__info}>
+                                <p>Upload File: JPG, PNG, WEBM Max 10MB</p>
+                                <div className={Style.voter__container__box__div__image}>
+                                  <Image src={images.candidate} width={150} height={150} objectFit='contain' alt='File upload'/>
+                                </div>
+                                <p>Drag & Drop File</p>
+                                <p>or Browse Media on your device</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={Style.input__container}>
+                <input 
+                  inputType="text" 
+                  title='Address' 
+                  placeholder='Voter Address' 
+                  handleClick={(e) => setFormInput({...formInput, name: e.target.value})}
+                />
+            </div>
+        </div>
+    </div>
+  )
 
 };
 
