1c8f46a1dbad1217ed23cd95627b49b68267ae1a
Vrush2004 <vrushalichaube@gmail.com>
2024-10-08 21:22:33 +0530

    button

--- pages/allowed-voters.js
+++ pages/allowed-voters.js
@@ -105,25 +105,30 @@ const allowedVoters = () => {
             </div>
             <div className={Style.input__container}>
                 <input 
-                  inputType="text" 
-                  title='Name' 
-                  placeholder='Voter Name' 
-                  handleClick={(e) => setFormInput({...formInput, name: e.target.value})}
+                    inputType="text" 
+                    title='Name' 
+                    placeholder='Voter Name' 
+                    handleClick={(e) => setFormInput({...formInput, name: e.target.value})}
                 />
                 <input 
-                  inputType="text" 
-                  title='Address' 
-                  placeholder='Voter Address' 
-                  handleClick={(e) => setFormInput({...formInput, name: e.target.value})}
+                    inputType="text" 
+                    title='Address' 
+                    placeholder='Voter Address' 
+                    handleClick={(e) => setFormInput({...formInput, address: e.target.value})}
                 />
                 <input 
-                  inputType="text" 
-                  title='Position' 
-                  placeholder='Voter Position' 
-                  handleClick={(e) => setFormInput({...formInput, name: e.target.value})}
+                    inputType="text" 
+                    title='Position' 
+                    placeholder='Voter Position' 
+                    handleClick={(e) => setFormInput({...formInput, position: e.target.value})}
                 />
+                <div className={Style.Button}>
+                    <Button btnName="Authorized Voter" handleClick={() => {}}/>
+                </div>
             </div>
         </div>
+
+        {/*  */}
     </div>
   )
 
