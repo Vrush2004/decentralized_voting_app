6206a34225fc7a1b127bdf69e6c33e4d8bd1f663
Vrush2004 <vrushalichaube@gmail.com>
2024-10-11 12:32:30 +0530

    candidate

--- pages/candidate-registration.js
+++ pages/candidate-registration.js
@@ -34,6 +34,58 @@ const candidateRegistration = () => {
     maxSize: 5000000,
   })
 
+  // JSX PART
+  return (
+    <div className={Style.createVoter}>
+        <div>
+            {fileUrl && (
+              <div className={Style.voterInfo}>
+                  <img src={ fileUrl } alt='Voter Image'/>
+                  <div className={Style.voterInfo_paragraph}>
+                      <p>
+                        Name: <span>&nbps; {candidateForm.name}</span>
+                      </p>
+                      <p>
+                        Address: &nbps;<span> {candidateForm.address.slice(0,20)}</span>
+                      </p>
+                      <p>
+                        Age: &nbps;<span> {candidateForm.address.age}</span>
+                      </p>
+                  </div>
+              </div>
+            )}
+            {
+              !fileUrl && (
+                <div className='Style.sideInfo'>
+                  <div className='Style.sideInfo_box'>
+                      <h4>Create Candidate For Voting</h4>
+                      <p>
+                        Blockchain voting organition, provide ethereum Ecosystem !!!
+                      </p>
+                      <p className={Style.sideInfo_para}>Contract Candidate</p>
+                  </div>
+                  <div className={Style.card}>
+                      {/* {voterArray.map((el,i) => (
+                          <div key={i+1} className={Style.card_box}>
+                              <div className={Style.image}>
+                                <img src="" alt="Profile photo" />
+                              </div>
+
+                              <div className={Style.card_info}>
+                                <p>Name</p>
+                                <p>Address</p>
+                                <p>Details</p>
+                              </div>
+                          </div>
+                      ))} */}
+                  </div>
+                </div>
+              )
+            }
+        </div>
+    </div>
+  )
+
 };
 
 export default candidateRegistration;
