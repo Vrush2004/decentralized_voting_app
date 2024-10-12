a2b0b848fa5cf5961840f43d755fda7e4c79dda5
Vrush2004 <vrushalichaube@gmail.com>
2024-10-12 12:19:29 +0530

    Navbar

---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,72 @@
+import React, {useState, useContext} from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import {AiFillLock, AiFillUnlock} from 'react-icons/ai'
+
+// Internal import
+import {VotingContext} from "../../context/Voter"
+import Style from "../NavBar/NavBar.module.css"
+import loading from '../../assets/loading.gif'
+
+const NavBar = () => {
+    const {connectWallet, error, currentAccount} = useContext(VotingContext)
+
+    const [openNav, setOpenNav] = useState(true)
+
+    const openNavigation = ()=>{
+      if(openNav){
+        setOpenNav(false)
+      }
+      else if(!openNav){
+        setOpenNav(true)
+      }
+    }
+  return (
+    <div className={Style.navbar}>
+      {error === "" ? (
+        ""
+      ) : (
+        <div className={Style.message__box}>
+          <div className={Style.message}>
+            <p>{error}</p>
+          </div>
+        </div>
+      )}
+
+      <div className={Style.navbar_box}>
+        <div className={Style.title}>
+          <Link href={{pathname: '/'}}>
+              <Image src={loading} alt='logo' width={80} height={80}/>
+          </Link>
+        </div>
+
+        <div className={Style.connect}>
+          {currentAccount ? (
+            <div>
+              <div className={Style.connect_flex}>
+                <button onClick={()=> openNavigation()}>
+                  {currentAccount.slice(0,10)}..
+                </button>
+                {currentAccount && (
+                  <span>
+                    {openNav ? (
+                      <AiFillUnlock onClick={()=> openNavigation()}/>
+                    ) : (
+                      <AiFillLock onClick={()=> openNavigation()}/>
+                    )}
+                  </span>
+                )}
+              </div>
+            </div>
+          ) : (
+            <button onClick={()=>connectWallet()}>
+                Connect Wallet
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
